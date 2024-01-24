06b9019f84d9aa182ccf7f0b8c3d1dba613cab08
Paulo Henrique <phttm0501@gmail.com>
2024-01-24 11:49:49 -0400

    introduction to testing with jest and testing library

--- projeto/src/components/Posts/index.jsx
+++ projeto/src/components/Posts/index.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 
 import { PostCard } from "../PostCard";
 
-export const Posts = ({ posts }) => (
+export const Posts = ({ posts = [] }) => (
   <div className="posts">
     {posts.map((post) => (
       <PostCard
