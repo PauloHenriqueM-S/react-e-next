1db81fa3cffd43454a611ca34a9570533b678c30
Paulo Henrique <phttm0501@gmail.com>
2024-03-18 20:32:53 -0400

    Bug

---
+++ projeto-2/src/templates/App/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import { PostsProvider } from '../../contexts/PostsProvider';
+import { Posts } from '../../components/Posts';
+
+function App() {
+  return (
+    <PostsProvider>
+      <div>
+        <Posts />
+      </div>
+    </PostsProvider>
+  );
+}
+
+export default App;
