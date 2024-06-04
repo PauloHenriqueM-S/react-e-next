4308058a6a214761d5933860be8cb8e78a834998
Paulo Henrique <phttm0501@gmail.com>
2024-06-04 14:01:26 -0400

    organization

--- projeto-2/src/components/Posts/index.jsx
+++ projeto-2/src/components/Posts/index.jsx
@@ -1,11 +1,20 @@
 import { useContext, useEffect, useRef } from 'react';
+import {
+  decrementCounter,
+  incrementCounter,
+} from '../../contexts/CounterProvider/action';
+import { CounterContext } from '../../contexts/CounterProvider/context';
 import { PostsContext } from '../../contexts/PostsProvider/context';
 import { loadPosts } from '../../contexts/PostsProvider/actions';
 
 export const Posts = () => {
   const isMounted = useRef(true);
+
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
+
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
 
   useEffect(() => {
     loadPosts(postsDispatch).then((dispatch) => {
@@ -20,6 +29,12 @@ export const Posts = () => {
 
   return (
     <div>
+      <button onClick={() => incrementCounter(counterDispatch)}>
+        Counter {counterState.counter}+
+      </button>
+      <button onClick={() => decrementCounter(counterDispatch)}>
+        Counter {counterState.counter}-
+      </button>
       <h1>Posts</h1>
       {postsState.loading && <p>Carregando posts...</p>}
 
